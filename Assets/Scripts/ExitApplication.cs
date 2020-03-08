c058b42f1f2d0cf939ca2f2d7bdcfa5224abc24e
Ann <antuanette.loo@gmail.com>
2020-03-07 21:33:08 -0500

    added escape button

---
+++ Assets/Scripts/ExitApplication.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitApplication : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
